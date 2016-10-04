d762a4f0fa65781bc06466f48b4a7142de28b896
aw <atatwata@gmail.com>
2016-10-04 22:45:45 +0900

    modify db access

--- routes/index.js
+++ routes/index.js
@@ -18,8 +18,15 @@ router.get('/mongo', function(req, res, next) {
   var url = 'mongodb://localhost:27017/test';
   // Connect using MongoClient
   MongoClient.connect(url, function(err, db) {
-    // Get an additional db
-    db.close();
+    // Use the admin database for the operation
+    var adminDb = db.admin();
+    // List all the available databases
+    adminDb.listDatabases(function(err, dbs) {
+      test.equal(null, err);
+      test.ok(dbs.databases.length > 0);
+      console.log(dbs);
+      db.close();
+    });
   });
   
   res.render('mongo', { title: 'mongo sample', content: 'content sample'});
