7b13ed5d97181d0f764bee7eb7b31392cea9315d
aw <atatwata@gmail.com>
2016-10-11 22:32:25 +0900

    add done files

--- routes/index.js
+++ routes/index.js
@@ -31,4 +31,26 @@ router.get('/mongo', function(req, res, next) {
   
 });
 
+router.get('/done', function(req, res, next) {
+
+  var MongoClient = require('mongodb').MongoClient,
+    test = require('assert');
+
+  // Connection url
+  var url = 'mongodb://localhost:27017/sample';
+
+  // Connect using MongoClient
+  MongoClient.connect(url, function(err, db) {
+    var col = db.collection('sample');
+    col.find({}).toArray(function(err, items){
+      test.equal(null,err);
+      console.log(items);
+      res.render('done', { title: 'done sample', content: JSON.stringify(items)});
+      db.close();
+    });
+  });
+  
+});
+
+
 module.exports = router;
